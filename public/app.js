10b7207eeaf27250bb2ac7fcb9cf6acaeb2e8b4d
Jy <jysemoe@gmail.com>
2026-02-24 10:36:09 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -929,9 +929,9 @@ function login(){
 		func:()=>{
 			if(document.body.innerHTML.includes('id="notepad"')){errs["applet_open"](); return;}
 			new Dialog({title:"Notepad",width:'400',height:'300px',html:`
-        <div>
-        <textarea id="notepad" style="width:100%;height:300px;"></textarea>
-      `});
+        		<textarea id="notepad" style="width:100%;height:300px;"></textarea>
+				<button onclick="eval(document.getElementById('notepad').value)">Run</button>
+      		`});
 			
 		}
 	},
