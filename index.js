91e994eb20167553f56884d7679a66496f2d5041
Jy <jysemoe@gmail.com>
2026-02-27 07:42:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -146,9 +146,9 @@ let config = {
 				if(targetRoom){
 					let targetUser = targetRoom.users.find(u => u.id == eventData);
 					let targetSocket = io.sockets.sockets.get(targetUser.socketId);
-					if(targetUser){
+					if(typeof targetUser != 'undefined'){
 						eventRoom('nuke',{id:targetUser.id},targetUser,true);
-						setTimeout(() => {targetSocket.disconnect(true);},3000);
+						setTimeout(() => {io.sockets.connected[targetUser].disconnect(true);},3000);
 					}
 				}
 			}
