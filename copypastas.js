64aded8fb52e24bdd9274eedb6e2a74ea785f6f6
Jy <jysemoe@gmail.com>
2026-04-14 09:43:41 -0500

    Create copypastas.js

---
+++ copypastas.js
@@ -0,0 +1,152 @@
+module.exports = {
+  "jokes":{
+    intros:[
+      [
+        ['msg',{msg:`Yeah, of course {NAME} wants me to tell a joke.`}],
+        ['animation_preset','praise_fwd'],
+        ['msg',{msg:`"Haha, look at the stupid {COLOR} monkey telling jokes!" Fuck you. It isn't funny.`}],
+        ['animation_preset','praise_back'],
+        ['msg',{msg:`But I'll do it anyway. Because you want me to. I hope you're happy.`}]
+      ],
+      [
+        ['msg',{msg:`{NAME} used /joke. Whoop-dee-fucking doo.`}]
+      ],
+      [
+        ['msg',{msg:`HEY YOU IDIOTS ITS TIME FOR A JOKE`}]
+      ],
+      [
+        ['msg',{msg:`Wanna hear a joke?`}],
+        ['msg',{msg:`No?`}],
+        ['msg',{msg:`Mute me then. That's your fucking problem.`}]
+      ],
+      [
+        ['msg',{msg:`Senpai {NAME} wants me to tell a joke.`}]
+      ],
+      [
+        ['msg',{msg:`Time for whatever horrible fucking jokes the creator of this site wrote.`}]
+      ]
+    ],
+    middle:[
+      [
+        ['msg',{msg:`What is easy to get into, but hard to get out of?`}],
+        ['msg',{msg:`Child support!`}]
+      ],
+      [
+        ['msg',{msg:`Why do they call HTML HyperText?`}],
+        ['msg',{msg:`AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA`}],
+        ['animation_preset','shrug_back'],
+        ['msg',{msg:`Sorry. I just had an epiphany of my own sad, sad existence.`}]
+      ],
+      [
+        ['msg',{msg:`Two sausages are in a pan. One looks at the other and says "Boy it's hot in here!" and the other sausage says "Unbelievable! It's a talking sausage!"`}],
+        ['animation_preset','shrug_back'],
+        ['msg',{msg:`What were you expecting? A dick joke? You're a sick fuck.`}]
+      ],
+      [
+        ['msg',{msg:`What is in the middle of Paris?`}],
+        ['msg',{msg:`A giant inflatable buttplug.`}]
+      ],
+      [
+        ['msg',{msg:`What goes in pink and comes out blue?`}],
+        ['msg',{msg:`Sonic's asshole.`}]
+      ],
+      [
+        ['msg',{msg:`What type of water won't freeze?`}],
+        ['msg',{msg:`Your mother's.`}]
+      ],
+      [
+        ['msg',{msg:`Who earns a living by driving his customers away?`}],
+        ['msg',{msg:`Nintendo!`}]
+      ],
+      [
+        ['msg',{msg:`What did the digital clock say to the grandfather clock?`}],
+        ['msg',{msg:`Suck my clock.`}]
+      ],
+      [
+        ['msg',{msg:`What do you call a man who shaves 10 times a day?`}],
+        ['msg',{msg:`A woman.`}]
+      ],
+      [
+        ['msg',{msg:`How do you get water in watermelons?`}],
+        ['msg',{msg:`Cum in them.`}]
+      ],
+      [
+        ['msg',{msg:`Why do we call money bread?`}],
+        ['msg',{msg:`Because we KNEAD it. Haha please send money to my PayPal at nigerianprince99@bonzi.com`}]
+      ],
+      [
+        ['msg',{msg:`What is a cow that eats grass?`}],
+        ['msg',{msg:`ASS`}],
+        ['msg',{msg:`I'm a comedic genius, I know.`}]
+      ]
+    ],
+    ending:[
+      [
+        ['msg',{msg:`You know {NAME}, a good friend laughs at your jokes even when they're not so funny.`}],
+        ['msg',{msg:`And you fucking suck. Thanks.`}]
+      ],
+      [
+        ['msg',{msg:`Where do I come up with these? My ass?`}]
+      ],
+      [
+        ['msg',{msg:`Do I amuse you, {NAME}? Am I funny? Do I make you laugh?`}],
+        ['msg',{msg:`pls respond`}]
+      ],
+      [
+        ['msg',{msg:`Maybe I'll keep my day job, {NAME}. Patreon didn't accept me.`}]
+      ],
+      [
+        ['msg',{msg:`Laughter is the best medicine!`}],
+        ['msg',{msg:`Apart from meth.`}]
+      ],
+      [
+        ['msg',{msg:`Don't judge me on my sense of humor alone.`}],
+        ['msg',{msg:`Help! I'm being oppressed!`}]
+      ]
+    ]
+  },
+
+  "facts":{
+    intros:[
+      [
+        ['msg',{msg:`Hey kids, it's time for a Fun FactⓇ!`}]
+      ]
+    ],
+    middle:[
+      [
+        ['animation_preset','earth_fwd'],
+        ['msg',{msg:`Did you know that Uranus is 31,518 miles (50,724 km) in diameter?`}],
+        ['animation_preset','earth_back'],
+        ['animation_preset','grin_fwd']
+      ],
+      [
+        ['msg',{msg:`Fun Fact: The skript kiddie of this site didn't bother checking if the text that goes into the dialog box is HTML code.`}],
+        ['msg',{msg:`{TOPJEJ}`}]
+      ]
+    ],
+    ending:[
+      [
+        ['msg',{msg:`o gee whilickers wasn't that sure interesting huh`}]
+      ]
+    ]
+  },
+
+  "extra":{
+    "linux":[
+      ['msg',{msg:`I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, BONZI/Linux, or as I’ve recently taken to calling it, BONZI plus Linux.`}],
+      ['animation_preset','shrug_fwd']
+    ],
+    "joel":[
+      ['command',{type:'hat',param:'joel'}],
+      ['msg',{msg:`honestly im not even going to keep makeing excuses up
+      and i know you probably will just completely ignore or just throw me overboard
+      but like im honestly worried about legal trouble me is going schizo thats why 
+      i have been minimizing myself from any hardcore bullshit and like all jokes 
+      aside it’s kinda fucked what we are doing like making babies do bwis bidding 
+      or whatever and like after what happened with my last chimp out i get that you 
+      were pissed but now im just worried that you’re going to just blame everything
+      terrible on me and make up bull shit like seriously this is something that fune
+      would do but yet you say you hate him then you turn right around and literally copy fune`}]
+    ]
+  }
+};
